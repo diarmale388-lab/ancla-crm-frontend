a7a01ae4b10971bc8566680d0cca48f9e5449208
Diego Machado <diarmale388@gmail.com>
2026-08-03 21:05:30 -0500

    Set relative base path for universal deployment in Hostinger

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // Ruta relativa universal para funcionar en raíz o en subcarpeta /dist/
   plugins: [react()],
   build: {
     rollupOptions: {
