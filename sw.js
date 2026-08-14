c5b1404d15eef8b840fa8069b269de6c0632c8f1
Diego Machado <diarmale388@gmail.com>
2026-08-14 13:56:00 -0500

    Force Chrome PWA install modal icon cache busting with new URLs ancla_app_icon_192.png and ancla_app_icon_512.png v1.2.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// ANCLA Special Projects - Service Worker PWA v1.1.9
-const CACHE_NAME = 'ancla-crm-cache-v1.1.9';
+// ANCLA Special Projects - Service Worker PWA v1.2.0
+const CACHE_NAME = 'ancla-crm-cache-v1.2.0';
 const STATIC_ASSETS = [
   '/',
   '/manifest.webmanifest',
